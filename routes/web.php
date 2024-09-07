d130ae18e9e43fe3af17cd6338fb152cc88fdae5
irfanyazid-13032000 <irfanyazid18@gmail.com>
2024-09-07 11:58:41 +0700

    edit routing

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\FilmController;
-use App\Http\Controllers\ArtistsController;
-use App\Http\Controllers\GalleryController;
-use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,24 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/film', [FilmController::class, 'index'])->name('films.index');
-Route::get('/film/create', [FilmController::class, 'create']);
-Route::post('/film/create', [FilmController::class, 'store']);
-Route::get('/film/destroy/{id}', [FilmController::class, 'destroy']);
-
-
-
-Route::get('/artists', [ArtistsController::class, 'index'])->name('artists.index');
-Route::get('/artists/create', [ArtistsController::class, 'create']);
-Route::post('/artists/create', [ArtistsController::class, 'store']);
-Route::get('/artists/destroy/{id}', [ArtistsController::class, 'destroy']);
-Route::get('/artists/edit/{id}', [ArtistsController::class, 'edit']);
-Route::get('/artists/{id}/profile', [ArtistsController::class, 'show']);
-Route::post('/artists/edit/{id}', [FilmController::class, 'update']);
-Route::get('/artists/list', [ArtistsController::class, 'getArtists'])->name('artists.list');
-
-
-Route::get('/gallery', [GalleryController::class, 'index']);
 
 
 
@@ -53,23 +31,3 @@ Route::get('/item/destroy/{id}', [ItemController::class, 'destroy'])->name('item
 Route::get('/allitem', [ItemController::class, 'getItems'])->name('items.all');
 
 
-
-Route::get('/film/list', [FilmController::class, 'getFilms'])->name('films.list');
-Route::get('/film/edit/{id}', [FilmController::class, 'edit']);
-Route::post('/film/edit/{id}', [FilmController::class, 'update']);
-
-
-Route::get('/produk', [ProdukController::class, 'index'])->name('produk');
-Route::get('/getproducts', [ProdukController::class, 'getProducts'])->name('get.produk');
-
-
-Route::get('/inject', function(){
-    return view('inject');
-});
-Route::get('/wadah', function(){
-    return view('tempat');
-});
-
-
-
-Route::get('/allfilm', [FilmController::class, 'allfilm']);
